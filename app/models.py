e5ac1c2c4aa5832e7764bd7d3b06ea2dfd836f48
sara <riad52166@gmail.com>
2023-12-30 22:38:13 +0200

    Crinkle

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=50)
-    details = models.TextField()
+   
     imga = models.ImageField(upload_to= 'imges/')
     def __str__(self):
         return self.name
